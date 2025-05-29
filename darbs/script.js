6c9daa6737a0ec944ac9a73f6a092721e3c65a7d
skibidigusiks <bujansgustavs@gmail.com>
2025-05-29 10:01:14 +0000

    fb

--- darbs/script.js
+++ darbs/script.js
@@ -5,14 +5,20 @@ function black() {
     console.log(random)
 }
 document.getElementById("rand1").addEventListener("dblclick", funkcija);
+document.getElementById("rand2").addEventListener("dblclick", funkcija2);
+document.getElementById("rand3").addEventListener("dblclick", funkcija3);
 let skaitlis1;
 let skaitlis2;
 let skaitlis3;
 function funkcija() {
     skaitlis1 = Math.floor(Math.random() * (1422 - 69 + 1) ) + 69;
-    skaitlis2 = Math.floor(Math.random() * (1422 - 69 + 1) ) + 69;
-    skaitlis3 = Math.floor(Math.random() * (1422 - 69 + 1) ) + 69;
     document.getElementById("rand1").innerHTML = skaitlis1;
+}
+function funkcija2() {
+    skaitlis2 = Math.floor(Math.random() * (1422 - 69 + 1) ) + 69;
     document.getElementById("rand2").innerHTML = skaitlis2;
+}
+function funkcija3() {
+    skaitlis3 = Math.floor(Math.random() * (1422 - 69 + 1) ) + 69;
     document.getElementById("rand3").innerHTML = skaitlis3;
 }
